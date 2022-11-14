3fa8a602deb62f3c943731c2c97c917fb597c05c
Devlin <oDEVLINo1o@hotmail.com>
2022-11-14 18:20:28 +0000

    first commit

---
+++ src/components/pet/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Pet = ({ animal }) => {
+  return (
+    <Link
+      to={`/${animal.type.toLowerCase()}/${animal.id}`}
+      key={animal.id}
+      className="pet"
+    >
+      <article>
+        <div className="pet-image-container">
+          {
+            <img
+              className="pet-image"
+              src={
+                animal.photos[0]?.medium || 'https://i.imgur.com/aEcJUFK.png'
+              }
+              alt=""
+            />
+          }
+        </div>
+        <h3>{animal.name}</h3>
+        <p>Breed: {animal.breeds.primary}</p>
+        <p>Color: {animal.colors.primary}</p>
+        <p>Gender: {animal.gender}</p>
+      </article>
+    </Link>
+  );
+};
+
+export default Pet;
